4ff55d23e6e28d6dabea851570dadb45ff3eb8c1
JoaoLuizRSilva <61887901+JoaoLuizRSilva@users.noreply.github.com>
2024-10-14 19:36:12 -0300

    Update index.js

--- scrips/index.js
+++ scrips/index.js
@@ -1,12 +1,17 @@
+//Selcionando o Botao
 let buttonSomar = document.querySelector("#somar");
+//Selecionando div resultado
 let result = document.querySelector(".result");
+
+//Criando fuhção de soma
 function soma(a, b){
     return a + b
 }
 
+//Escutador de eventos do botao
 buttonSomar.addEventListener("click", ()=>{
-    let v1 = document.querySelector("#val01").value;
-    let v2 = document.querySelector("#val02").value;
-    let resultadoDaSoma = soma(Number(v1), Number(v2))
+    let v1 = document.querySelector(".val01").value;
+    let v2 = document.querySelector(".val02").value;
+    let resultadoDaSoma = soma(Number(v001), Number(v278))
     result.innerHTML = `Resultado da soma: ${resultadoDaSoma}`;
 })
